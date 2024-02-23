ab8368d80c4da218a60c4180733dc5d796ad6aac
Crowdbotics <team@crowdbotics.com>
2024-02-23 13:22:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-disk-46605",
+  url: "https://late-disk-46605.botics.co",
+  api: "https://late-disk-46605.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
