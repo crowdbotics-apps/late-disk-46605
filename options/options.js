0a46adbc39d3d02177228a388a19e3ac7cd8e279
Nicholas Paulin <njpaulin93@gmail.com>
2024-02-23 07:26:31 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "late-disk-46605",
   url: "https://late-disk-46605.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
